5fd306fe9631fe9583fe9d253eaacf721ec65101
pawida chumpurat <pawida.may@gmail.com>
2023-07-25 15:47:44 +0700

    feat(ChatsPage):add react-chat-engine-advanced

--- client/src/ChatsPage.jsx
+++ client/src/ChatsPage.jsx
@@ -1,4 +1,12 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic('6b98ffff-ea38-460f-9814-33079e3f1abc', props.user.username, props.user.secret)
+    return (
+        <div style={{ height: '100vh'}}>
+            <MultiChatSocket {...chatProps}/>
+            <MultiChatWindow {...chatProps} style={{ height: '100%'}}/>
+        </div>
+    )
 }
 export default ChatsPage
