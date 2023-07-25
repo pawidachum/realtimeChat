9b7827d82aeb7f955a74a6679c8d9dd21b213d72
pawida chumpurat <pawida.may@gmail.com>
2023-07-25 15:00:24 +0700

    feat(AuthPage): add axios fetch to server

--- client/src/AuthPage.jsx
+++ client/src/AuthPage.jsx
@@ -1,7 +1,11 @@
+import axios from 'axios'
 const AuthPage = (props) => {
     const onSubmit = (e) => {
       e.preventDefault();
       const { value } = e.target[0];
+      axios.post('http://localhost:3001/authenticate', {username: value})
+      .then(r => props.onAuth({ ...r.data, secret: value }))
+      .catch(e => console.log('error', e))
       props.onAuth({ username: value, secret: value });
     };
   
